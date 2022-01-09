077cd9d899b6d76cfd7b425283cc9b253b923398
Josh Long <josh@joshlong.com>
2022-01-08 16:59:19 -0800

    polish security

--- security/src/main/java/rsb/rsocket/security/GreetingRequest.java
+++ security/src/main/java/rsb/rsocket/security/GreetingRequest.java
@@ -1,14 +1,4 @@
 package rsb.rsocket.security;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class GreetingRequest {
-
-	private String name;
-
+public record GreetingRequest(String name) {
 }
--- security/src/main/java/rsb/rsocket/security/GreetingResponse.java
+++ security/src/main/java/rsb/rsocket/security/GreetingResponse.java
@@ -1,14 +1,4 @@
 package rsb.rsocket.security;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class GreetingResponse {
-
-	private String message;
-
+public record GreetingResponse(String message) {
 }
